c5dc033bcbe64b3740bdac5c5f9040039fa84e29
manoj <manojsurya5616@gmail.com>
2025-08-07 11:12:31 +0530

    unify multiplication and substraction

--- calculator.py
+++ calculator.py
@@ -13,11 +13,7 @@ if __name__ == "__main__":
     calc = Calculator()
     print("Welcome to Calculator!")
 
-    print("Current features: Addition, Multiplication")
+    print("Current features: Addition, Multiplication, Subtraction")
     print("Example: 5 + 3 =", calc.add(5, 3))
     print("Example: 5 * 3 =", calc.multiply(5, 3))
-
-    print("Current features: Addition, Subtraction")
-    print("Example: 5 + 3 =", calc.add(5, 3))
     print("Example: 5 - 3 =", calc.subtract(5, 3))
-
